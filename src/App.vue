c31c92204dccae16680b3a7d18a1ab4f3c53714a
DESKTOP-92AB27R\admin <15735183360@163.com>
2021-06-11 17:24:47 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    
     <router-view/>
   </div>
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
+li{list-style:none}
+img{vertical-align:top;border:none}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background: #FFFFFF;
+}
+@media screen and (max-width: 1441000px) and (min-width: 1441px) {
+  .side{
+    padding: 0 104px;
+  }
+}
+@media screen and (max-width: 1440px) and (min-width: 1069px) {
+  .side{
+    padding: 0 78px;
+  }
+}
+@media screen and (max-width: 1068px) and (min-width: 768px) {
+  .side{
+    padding: 0 58px;
+  }
+}
+/* 手机端的样式 */
+@media screen and (max-width: 767px) and (min-width: 0px) {
+  .side{
+    padding: 0 41px;
+  }
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/head.vue
@@ -0,0 +1,179 @@
+<template>
+  <div class="banner-bg side">
+    <ul class="head-nav">
+      <li class="head-nav-icon">
+        <img
+          src="https://lanhu.oss-cn-beijing.aliyuncs.com/SketchPng45db1181188c260b408a80d168cbc8eeba45187d6668dcc8824821166660dea7"
+        />" alt="">
+        <span>MonYa</span>
+      </li>
+      <li class="head-nav-btn">
+        <span class="hot">Home</span>
+        <span>About us</span>
+      </li>
+      <li class="head-nav-contact">Contact us</li>
+    </ul>
+    <div class="head-content">
+      Mengya is an e-commerce wholesale platform focusing on Chinese GK and
+      statues, Serving distributors from all over the world.
+    </div>
+    <div class="head-bottom-btn">Contact us</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      msg: "",
+    };
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.banner-bg {
+  background: url("../assets/banner.png") no-repeat center/100% 100%;
+  position: relative;
+}
+.head-nav {
+  height: 80px;
+  position: relative;
+}
+.head-nav-icon {
+  width: 130px;
+  height: 48px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  left: 0;
+}
+.head-nav-icon > img {
+  width: 48px;
+  height: 48px;
+  position: absolute;
+  left: 0;
+}
+.head-nav-icon > span {
+  font-size: 20px;
+  font-family: DFWaWaTC-W5;
+  color: #ffffff;
+  line-height: 48px;
+  position: absolute;
+  right: 0;
+}
+.head-nav-btn {
+  height: 48px;
+  line-height: 48px;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  left: 250px;
+}
+.head-nav-btn > span {
+  margin-left: 40px;
+  font-size: 20px;
+  color: #ffffff;
+  opacity: 0.65;
+}
+.head-nav-btn > span:nth-child(1) {
+  margin-left: 0;
+}
+.head-nav-btn > span.hot {
+  font-weight: 600;
+  opacity: 1;
+}
+.head-nav-contact {
+  width: 144px;
+  height: 48px;
+  border-radius: 24px;
+  border: 1px solid #ffffff;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  right: 0;
+  font-size: 20px;
+  color: #ffffff;
+  text-align: center;
+  line-height: 48px;
+}
+.head-content {
+  position: absolute;
+  color: #ffffff;
+  text-align: left;
+}
+.head-bottom-btn {
+  width: 246px;
+  height: 58px;
+  background: #e14d48;
+  box-shadow: 0px 2px 6px 0px rgba(149, 31, 27, 0.6);
+  border-radius: 29px;
+  position: absolute;
+  font-size: 32px;
+  font-weight: bold;
+  color: #ffffff;
+  text-align: center;
+  line-height: 58px;
+  text-shadow: 0px 0px 2px rgba(225, 77, 72, 0.4);
+}
+@media screen and (max-width: 1930px) and (min-width: 1441px) {
+  .banner-bg {
+    height: 800px;
+  }
+  .head-content {
+    width: 853px;
+    top: 213px;
+    font-size: 54px;
+    line-height: 64px;
+  }
+  .head-bottom-btn{
+    bottom:197px;
+  }
+}
+@media screen and (max-width: 1440px) and (min-width: 1069px) {
+  .banner-bg {
+    height: 600px;
+  }
+  .head-content {
+    width: 640px;
+    top: 160px;
+    font-size: 40px;
+    line-height: 48px;
+  }
+  .head-bottom-btn{
+    bottom:148px;
+  }
+}
+@media screen and (max-width: 1068px) and (min-width: 768px) {
+  .banner-bg {
+    height: 445px;
+  }
+  .head-content {
+    width: 475px;
+    top: 119px;
+    font-size: 30px;
+    line-height: 34px;
+  }
+  .head-bottom-btn{
+    bottom:110px;
+  }
+}
+/* 手机端的样式 */
+@media screen and (max-width: 767px) and (min-width: 0px) {
+  .banner-bg {
+    height: 320px;
+  }
+  .head-content {
+    top: 85px;
+  }
+  .head-bottom-btn{
+    bottom:79px;
+  }
+}
+</style>
